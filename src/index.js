8c364d4afecfe51373b68838066fa106b648ab37
kyzne <kyznetsovstepa@gmail.com>
2023-03-12 19:56:53 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -45,7 +45,7 @@ function postComment(e) {
                 <span class = "solo-comments__name">
                     ${name.value.trim()}
                 </span>
-                <span  class = "solo-comments__date">
+                <span class = "solo-comments__date">
                     ${getDaysDifference()}, ${getTime()}
                 </span>
                 <p class = "solo-comments__text">
@@ -55,8 +55,41 @@ function postComment(e) {
         </div>
         <div class = "solo-comments__actions">
             <span class = "solo-comments__likesCounter">0</span>
-            <img id = "like" class = "solo-comments__like" src = "${imagesPath}/like.svg" alt = "like">
-            <img data-del="delete" src = "${imagesPath}/delete.svg" alt="delete">
+            <svg class = "solo-comments__like"
+                width="30px"
+                height="30px"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path class="stroke-like" d="M8 10V20M8 10L4 9.99998V20L8 20M8 10L13.1956
+                      3.93847C13.6886 3.3633 14.4642 3.11604 15.1992 3.29977L15.2467
+                      3.31166C16.5885 3.64711 17.1929 5.21057 16.4258 6.36135L14
+                      9.99998H18.5604C19.8225 9.99998 20.7691 11.1546 20.5216
+                      12.3922L19.3216 18.3922C19.1346 19.3271 18.3138 20 17.3604 20L8 20"
+                      stroke="#868d9a"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"/>
+            </svg>
+            <svg data-del="delete"
+                 width="30px"
+                 height="30px"
+                 viewBox="0 0 24 24"
+                 fill="none"
+                 xmlns="http://www.w3.org/2000/svg">
+                 <path opacity="0.15"
+                       d="M18 18V6H6V18C6 19.1046 6.89543
+                       20 8 20H16C17.1046 20 18 19.1046 18 18Z"
+                       fill="none"/>
+                 <path d="M10 10V16M14 10V16M18 6V18C18 19.1046
+                       17.1046 20 16 20H8C6.89543 20 6 19.1046 6 18V6M4
+                       6H20M15 6V5C15 3.89543 14.1046 3 13 3H11C9.89543 3 9 3.89543
+                       9 5V6"
+                       stroke="#868d9a"
+                       stroke-width="1.5"
+                       stroke-linecap="round"
+                       stroke-linejoin="round"/>
+            </svg>
         </div>
     </div>
     `
@@ -152,16 +185,17 @@ function getCommentsCounter() {
 function likesToggle() {
     let likeButtons = Array.from(document.querySelectorAll('.solo-comments__like'))
     let likeCounts = Array.from(document.querySelectorAll('.solo-comments__likesCounter'))
+    let icons = Array.from(document.getElementsByClassName("stroke-like"))
 
     likeButtons.forEach((button, index) => {
         if (i !== 0 && index !== 0) return
         
         button.addEventListener("click", () => {
+            icons[index].classList.toggle('active-svg')
             button.classList.toggle("is-active")
             const current = Number(likeCounts[index].innerHTML)
             const inc = button.classList.contains("is-active") ? 1 : -1
             likeCounts[index].innerHTML = current + inc
-            button.removeEventListener("click")
         })
     })
 //установка значения глобальной переменной с целью предотвращения повторной установки слушателя клика     
@@ -170,6 +204,10 @@ function likesToggle() {
 //все остальные обработчики событий
 commentText.addEventListener("beforeinput", () =>
     notice.style.visibility = "hidden")
+
+commentText.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") postComment(e) 
+})
 
 name.addEventListener("beforeinput", () =>
     notice.style.visibility = "hidden")
